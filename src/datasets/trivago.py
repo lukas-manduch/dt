ef8b8fd8a48548f691bfed239c4f73a04a12455b
Lukas Manduch <manduch.lukas@gmail.com>
2019-04-23 16:01:27 +0200

    Data preprocessing finished 	modified:   ../../requirements.txt 	modified:   trivago.py

--- src/datasets/trivago.py
+++ src/datasets/trivago.py
@@ -1,4 +1,5 @@
 import os
+import random
 from functools import partial, reduce
 from typing import Iterable
 
@@ -8,13 +9,16 @@ import scipy
 
 # import constants
 
-REL_TRAIN_PATH = "data/trivago_test.csv"
-REL_TEST_PATH = "data/trivago_train.csv"
-COLUMNS = ["user_id", "reference"]
+REL_TRAIN_PATH = "data/trivago_train.csv"
+REL_TEST_PATH = "data/trivago_test.csv"
+USER_ID = "user_id"
 CACHE_FOLDER = 'data'
+COLUMNS = [USER_ID, "reference"]
+ACTIONS = ["clickout item", "interaction item image"]
+
 
 def debug_print(*args, level=1, **kwargs):
-    if level<3:
+    if level<9:
         print(" "*2*level, end='')
         print(*args, **kwargs)
 
@@ -51,13 +55,58 @@ def __pandas_field_intersection(field_name, *datasets) -> Iterable:
 
 def __pandas_strip_columns(dataset, columns):
     useless = columns ^ set(dataset.columns)
-    debug_print("Deleting from dataset {}".format(columns), level=2)
+    debug_print("Deleting from dataset {}".format(useless), level=2)
+    debug_print("Dataset shape before {}".format(dataset.shape), level=3)
+    dataset = dataset.drop(useless, axis=1)
+    debug_print("Dataset shape after {}".format(dataset.shape), level=3)
+    return dataset
 
-def __pandas_strip_references(dataset):
-    pass
+def __pandas_trivago_invalid_rows(dataset):
+    action_type = "action_type"
+    assert action_type in dataset
+    debug_print("Preprocessing trivago", level=2)
+    debug_print("Dataset shape before {}".format(dataset.shape), level=3)
+    dataset = dataset[dataset[action_type].isin(ACTIONS)].dropna()
+    debug_print("Dataset shape after {}".format(dataset.shape), level=3)
+    return dataset
 
-def __pandas_only_users(dataset, user_ids):
-    pass
+def __pandas_trivago_drop_unique(dataset, column, percentage=0):
+    assert percentage >= 0 and percentage <= 1
+    unique_values = list(dataset[column].unique())
+    debug_print("Droping users, size before {} ({}%)".format(len(dataset),
+                                                             percentage*100), level=2)
+    debug_print("Unique users, before {}".format(len(unique_values)), level=3)
+
+    drop_num = int(len(unique_values) * percentage)
+    drop_values = random.sample(unique_values, drop_num)
+    result = dataset[~dataset[column].isin(drop_values)]
+
+    debug_print("Unique users, after {}".format(len(result[column].unique())), level=3)
+    debug_print("Droping users, size after {}".format(len(result)), level=2)
+    return result
+
+def __pandas_drop_top(dataset, column, percentage=0):
+    debug_print("Dropping top users {}%".format(percentage*100), level=2)
+    dataset['count'] = dataset.groupby(column).transform('count')
+    dataset.sort_values('count', inplace=True, ascending=False)
+    unique_users = dataset[column].unique()
+    drop_users = unique_users[:int(len(unique_users)*percentage)]
+    debug_print("Count {}".format(len(drop_users)), level=3)
+    debug_print("Largest before {}".format(list(dataset['count'])[0]), level=3)
+    dataset = dataset[~dataset[column].isin(drop_users)]
+    dataset = dataset[dataset['count'] > 4]
+    debug_print("Largest after {}".format(list(dataset['count'])[0]), level=3)
+    del dataset['count']
+    return dataset
+
+def __pandas_trivago_plot_density(dataset, column, filename=None):
+    data = dataset[column].value_counts()
+    import matplotlib.pyplot as plt
+    plt.hist(data)
+    # plt.ylim(top=100)
+    # plt.xlim(left=30, right=1400)
+    plt.show()
+
 ###############################################################
 
 def _script_relative(relative_path):
@@ -89,8 +138,12 @@ def hash_params(*args, **kwargs):
     return hash_recursive([args, kwargs])
 
 
-def get_trivago_datasets(columns, percentage=1, seed=1):
-    """"""
+def get_trivago_datasets(columns, percentage=1, seed=1,
+                         uitems_min=2, uitem_max=-1):
+    """Doc
+        - uitem_min - minimum number of interaction for user
+    """
+    debug_print("Loading trivago datasets", level=0)
     columns = set(columns + COLUMNS)
     # Compute names
     file_name = str(hash_params(columns, percentage, seed))
@@ -105,14 +158,19 @@ def get_trivago_datasets(columns, percentage=1, seed=1):
         test_path = _script_relative(REL_TEST_PATH)
 
         train = __pandas_get_dataset(train_path)
+        train = __pandas_trivago_invalid_rows(train)
         train = __pandas_strip_columns(train, columns)
-        train = __pandas_strip_dataset(train, fields=columns,
-                                       percentage=percentage, seed=seed)
+        __pandas_trivago_plot_density(train, USER_ID)
+        train = __pandas_trivago_drop_unique(train, USER_ID, percentage=0)
+        train = __pandas_drop_top(train, USER_ID, percentage=0.03)
 
         test = __pandas_get_dataset(test_path)
+        test = __pandas_trivago_invalid_rows(test)
+        test = __pandas_strip_columns(test, columns)
+        test = test[~test[USER_ID].isin(train[USER_ID].unique())]
         # Save dataset
-        train.to_pickle(path=train_path)
-        test.to_pickle(path=test_path)
+        #train.to_pickle(path=train_path)
+        #test.to_pickle(path=test_path)
     else: # Load dataset
         pass
 
@@ -125,3 +183,8 @@ if __name__ == "__main__":
 
     # print(len(__pandas_field_intersection('user_id', test, test)))
 
+def get_letter_type(letter):
+    if letter.lower() in 'aeiou':
+        return 'vowel'
+    else:
+        return 'consonant'
